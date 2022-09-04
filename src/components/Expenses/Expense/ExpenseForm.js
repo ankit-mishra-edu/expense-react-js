47a52714f95b062d2e6fe3a4c018658abc7428e3
ankit-mishra-edu <ankit.mishra.edu@gmail.com>
2022-09-04 15:20:52 +0530

    Dynamic Styling

--- src/components/Expenses/Expense/ExpenseForm.js
+++ src/components/Expenses/Expense/ExpenseForm.js
@@ -1,76 +1,150 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
+import FormInput from './FormInput';
+
 const ExpenseForm = (props) => {
   const [title, setTitle] = useState('');
   const [price, setPrice] = useState('');
   const [dateTime, setDateTime] = useState('');
 
-  const onChangeTitleHandler = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const onChangePriceHandler = (event) => {
-    setPrice(event.target.value);
-  };
-
-  const onChangeDateTimeHandler = (event) => {
-    setDateTime(event.target.value);
-  };
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
   const onSubmitFormHandler = (event) => {
     event.preventDefault();
+
+    setIsFormSubmitted(true);
+
+    if (!title || !price || !dateTime) {
+      return;
+    }
 
     const expense = {
       title: title,
       price: price,
       dateTime: dateTime,
     };
-    console.log(expense);
+
     props.onAddExpense(expense);
 
-    // Reset form value
-    setTitle('');
-    setPrice('');
-    setDateTime('');
+    // // Reset form value
+    // setTitle('');
+    // setPrice('');
+    // setDateTime('');
   };
 
   return (
     <form onSubmit={onSubmitFormHandler}>
       <legend>{props.children}</legend>
       <div className="expense_form">
+        <FormInput
+          type="text"
+          onInputValueChange={setTitle}
+          isFormSubmitted={isFormSubmitted}
+        />
+        <FormInput
+          type="number"
+          onInputValueChange={setPrice}
+          isFormSubmitted={isFormSubmitted}
+        />
+        <FormInput
+          type="date"
+          onInputValueChange={setDateTime}
+          isFormSubmitted={isFormSubmitted}
+        />
         <div className="expense_form__item">
-          <label className="expense_form__label">Title: </label>
-          <input
-            type="text"
-            value={title}
-            className="expense_form__input"
-            onChange={onChangeTitleHandler}
-          />
-        </div>
-        <div className="expense_form__item">
-          <label className="expense_form__label">Price: </label>
-          <input
-            type="number"
-            value={price}
-            className="expense_form__input"
-            onChange={onChangePriceHandler}
-          />
-        </div>
-        <div className="expense_form__item">
-          <label className="expense_form__label">DateTime: </label>
-          <input
-            type="date"
-            value={dateTime}
-            className="expense_form__input"
-            onChange={onChangeDateTimeHandler}
-          />
-        </div>
-        <div className="expense_form__item">
-          <button type="submit">Add Expense</button>
+          <button type="submit" className="expense_form__submit_button">
+            Add Expense
+          </button>
         </div>
       </div>
     </form>
   );
 };
 export default ExpenseForm;
+
+// import React, { useState } from 'react';
+// import './ExpenseForm.css';
+
+// const ExpenseForm = (props) => {
+//   const [title, setTitle] = useState('');
+//   const [price, setPrice] = useState('');
+//   const [dateTime, setDateTime] = useState('');
+
+//   let [isInputValid, setisInputvalid] = useState(true);
+
+//   const onChangeTitleHandler = (event) => {
+//     setTitle(event.target.value);
+//   };
+
+//   const onChangePriceHandler = (event) => {
+//     setPrice(event.target.value);
+//   };
+
+//   const onChangeDateTimeHandler = (event) => {
+//     setDateTime(event.target.value);
+//   };
+
+//   const onSubmitFormHandler = (event) => {
+//     event.preventDefault();
+
+//     if (title.trim().length === 0) {
+//       setisInputvalid(false);
+//       return;
+//     }
+
+//     const expense = {
+//       title: title,
+//       price: price,
+//       dateTime: dateTime,
+//     };
+//     console.log(expense);
+//     props.onAddExpense(expense);
+
+//     // Reset form value
+//     setTitle('');
+//     setPrice('');
+//     setDateTime('');
+//   };
+
+//   return (
+//     <form onSubmit={onSubmitFormHandler}>
+//       <legend>{props.children}</legend>
+//       <div className="expense_form">
+//         <div className={`expense_form__item ${isInputValid ? '' : 'invalid'}`}>
+//           <label className="expense_form__label">Title: </label>
+//           <input
+//             type="text"
+//             value={title}
+//             className="expense_form__input"
+//             onChange={onChangeTitleHandler}
+//           />
+//         </div>
+//         <div className={`expense_form__item ${isInputValid ? '' : 'invalid'}`}>
+//           <label className="expense_form__label">Price: </label>
+//           <input
+//             type="number"
+//             value={price}
+//             className="expense_form__input"
+//             onChange={onChangePriceHandler}
+//           />
+//         </div>
+//         <div className={`expense_form__item ${isInputValid ? '' : 'invalid'}`}>
+//           <label className="expense_form__label">DateTime: </label>
+//           <input
+//             type="date"
+//             value={dateTime}
+//             className="expense_form__input"
+//             onChange={onChangeDateTimeHandler}
+//           />
+//         </div>
+//         <div className="expense_form__item">
+//           <button type="submit" className="expense_form__submit_button">
+//             Add Expense
+//           </button>
+//         </div>
+//       </div>
+//     </form>
+//   );
+// };
+// export default ExpenseForm;
---
+++ src/components/Expenses/Expense/FormInput.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './FormInput.css';
+
+const FormInput = (props) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputValueChangeHandler = (event) => {
+    setInputValue(event.target.value.trim());
+    props.onInputValueChange(event.target.value);
+  };
+
+  return (
+    <div
+      className={`expense_form__item ${
+        props.isFormSubmitted && inputValue.length === 0 ? 'invalid' : ''
+      }`}
+    >
+      <label className="expense_form__label">Title: </label>
+      <input
+        type={props.type}
+        value={inputValue}
+        className="expense_form__input"
+        onChange={onInputValueChangeHandler}
+      />
+    </div>
+  );
+};
+
+export default FormInput;
